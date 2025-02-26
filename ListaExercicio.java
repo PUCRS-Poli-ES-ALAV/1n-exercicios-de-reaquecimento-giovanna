8c6d619ec1471ec95218d80ec87b0ab78fd3e773
Giovanna Castro <gihcastro1010@gmail.com>
2025-02-26 20:35:22 -0300

    inicio

---
+++ ListaExercicio.java
@@ -0,0 +1,51 @@
+public class ListaExercicio{
+
+public int multDeSomasSucessivas(int a, int b){
+    do{
+        int resultado = b + b;
+    }while (resultado < a);
+}
+
+public int somaDeIncrementosSucessivos(int a, int b){
+
+}
+
+public float calculoDeFracoes(int a){
+
+}
+
+public String inversaoDeString(String abc){
+
+}
+
+public int funcaoDeEckerman(int a, int b){
+    int resultado = b + 1;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
