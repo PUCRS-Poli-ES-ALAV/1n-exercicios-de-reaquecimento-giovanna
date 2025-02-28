f41a0eeac6309fe07d622aa91c00e3391293ef58
Giovanna Castro <gihcastro1010@gmail.com>
2025-02-28 01:53:53 -0300

    Primeirito! hehe

--- ListaExercicio.java
+++ ListaExercicio.java
@@ -1,27 +1,40 @@
 public class ListaExercicio{
 
-public int multDeSomasSucessivas(int a, int b){
-    do{
-        int resultado = b + b;
-    }while (resultado < a);
-}
-
-public int somaDeIncrementosSucessivos(int a, int b){
-
-}
-
-public float calculoDeFracoes(int a){
-
-}
-
-public String inversaoDeString(String abc){
-
-}
-
-public int funcaoDeEckerman(int a, int b){
-    int resultado = b + 1;
+public static int multDeSomasSucessivas(int a, int b){
+    // caso base primeiro
+    if (a == 0 || b == 0){
+        return 0;
+    }
+    if (b == 1){
+        return a;
+    }
+    if (a == 1){
+        return b;
+    }
     
+    return a + multDeSomasSucessivas(a, b-1);
 }
+
+public static void main(String[] args) {
+   System.out.println(multDeSomasSucessivas(4,3));
+}
+
+// public int somaDeIncrementosSucessivos(int a, int b){
+
+// }
+
+// public float calculoDeFracoes(int a){
+
+// }
+
+// public String inversaoDeString(String abc){
+
+// }
+
+// public int funcaoDeEckerman(int a, int b){
+//     int resultado = b + 1;
+    
+// }
 
 
 
